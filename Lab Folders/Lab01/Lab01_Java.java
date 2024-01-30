3a76fa61104f7413e1e9470637486b1f783d4c0c
Samuel Ogunkola <samuelogunkola10@gmail.com>
2024-01-30 12:42:35 +0000

    test commit

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-  //Edit made by Oluwademilade Ogunkola_220062619
+  //Edit made by Oluwademilade Ogunkola_220062619 in GitPod
 
   //Last line print('Good bye')
 }
